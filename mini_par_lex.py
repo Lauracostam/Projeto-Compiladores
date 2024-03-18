35931838a7af61dac4706ea035a2b73ed205b58b
laura costa <107508155+Lauracostam@users.noreply.github.com>
2024-03-18 14:55:53 -0300

    versão 2 analisadores

--- mini_par_lex.py
+++ mini_par_lex.py
@@ -1,6 +1,10 @@
-import ply.lex as lex
+#LEXER
 
-# Palavras-chave reservadas
+from ply import lex
+from ply.lex import TOKEN
+
+# Reserved tokens
+
 reserved = {
     'if': 'IF',
     'else': 'ELSE',
@@ -10,87 +14,93 @@ reserved = {
     'and': 'AND',
     'or': 'OR',
     'SEQ': 'SEQ',
-    'PAR': 'PAR'
+    'PAR': 'PAR',
+    'print' : "PRINT"
 }
 
-# Lista de tokens
-tokens = [
-    'ID', 'NUMBER', 'STRING', 'BOOLEAN',
-    'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-    'LESSTHAN', 'GREATERTHAN', 'EQUAL', 'NOTEQUAL',
-    'LPAREN', 'RPAREN', 'ASPAS', 'COMMENT', 'GREATEQ', 'LESSEQ'
-] + list(reserved.values())
+# List of token names.   This is always required
 
-# Expressões regulares para tokens simples
+tokens = [
+     'INT', 'FLOAT32', 'ID', 'LPAREN', 'RPAREN', 'BOOLEAN', 'COLON', 'SEMICOLON',
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'NUMBER', 'EQUALS', 'LESSTHAN', 'GREATERTHAN',
+    'NOTEQUAL', 'STRING', 'ERR_STRING', 'RCOMMA', 'COMMENT','GREATEQ', 'LESSEQ', 'EQUAL' ] + list(reserved.values())
+
+# Regular expression rules for simple tokens
+
+t_SEQ = r'SEQ'
+t_PAR = r'PAR'
+t_INT = r'int'
+t_FLOAT32 = r'float32'
+t_IF = r'if'
+t_ELSE = r'else'
+t_WHILE = r'while'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+#t_BOOLEAN = r'True|False'
+t_COLON = r':'
+t_SEMICOLON = r';'
+t_RCOMMA = r'\"'
+t_EQUALS = r'\='
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
-t_LESSTHAN = r'<'
-t_GREATERTHAN = r'>'
-t_EQUAL = r'='
-t_NOTEQUAL = r'!='
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_ASPAS = r'\"'
+t_LESSTHAN = r'\<'
+t_GREATERTHAN = r'\>'
+t_NOTEQUAL = r'\!\='
+t_GREATEQ = r'\>\='
+t_LESSEQ = r'\<\='
+t_EQUAL   = r'\=\='
+t_AND = r'and'
+t_OR = r'or'
 t_COMMENT = r'\#.*'
 
-# Ignorar espaços em branco e tabulações
-t_ignore = ' \t'
 
-# Tratamento de números inteiros
+# A regular expression rule with some action code
 def t_NUMBER(t):
     r'\d+'
-    t.value = int(t.value)
+    t.value = int(t.value)    
     return t
 
-# Tratamento de strings
 def t_STRING(t):
-    r'"[^"]*"'
+    r'("[^"]*")'
+    return t 
+
+def t_ERR_STRING(t):
+    r'"[^("|\n?)]*'
+    return t 
+
+def t_BOOLEAN(t): 
+    r'True|False'
+    t.type = 'BOOLEAN'
     return t
 
-# Tratamento de identificadores
 def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')  # Verifica palavras reservadas
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    if t.value in reserved:
+        t.type = reserved[t.value]
     return t
 
-# Tratamento de quebras de linha
+# Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# Tratamento de erros
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
+    print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-# Construa o analisador léxico
+# Build the lexer
 lexer = lex.lex()
 
-# Exemplo de teste
-example_input = '''
-SEQ
-    result = True
-    i = 1
-    WHILE (i <= 10)
-        IF (i>2 and i != 0 and result)
-            result = result and False
-        i = i + 1
-    #testando comentarios
-PAR
-    j = 1
-    WHILE (j <= 10)
-        IF (j>2 and j != 0 and result)
-            result = result and False
-        j = j + 1
-'''
+#teste
+data = "SEQ if (True) a = 5; else b = 10;"
 
-lexer.input(example_input)
+lexer.input(data)
 
-# Exiba os tokens encontrados
-while True:
-    tok = lexer.token()
-    if not tok:
-        break  # Não há mais entrada
+for tok in lexer:
     print(tok)
--- mini_par_sint.py
+++ mini_par_sint.py
@@ -1,31 +1,91 @@
 import ply.yacc as yacc
-from mini_par_lex import tokens    
+from lexer import tokens  
+
+precedence = (
+    ('left', 'IF', 'ELSE'),
+    ('left', 'WHILE'),
+    ('left','PLUS','MINUS'),
+    ('left','TIMES','DIVIDE'),
+    ('right','UMINUS'),
+)      
+            
+ids = { }
 
 def p_programa_minipar(p):
-    'programa_minipar : bloco_stmt'
-    p[0] = p[1]    
-     
-def p_bloco_stmt_stmts(p):
-    '''bloco_stmt : stmts''' 
+    'programa_minipar : bloco_stmt' 
+    
+def p_bloco_stmt_bloco(p):
+    '''bloco_stmt : stmts'''     
     p[0] = p[1] 
-   
+
 def p_stmts_stmt(p):
-    '''stmts : stmt'''
+    '''stmts : stmt'''   
     p[0] = p[1]
              
 def p_stmts_stmts_stmt(p):
     '''stmts : stmts stmt'''
     p[0] = p[1]    
-
-def p_stmt_expression(p):
-    '''stmt : expression'''
-    p[0] = p[1]
     
+def p_statement_assign(p):
+    'stmt : ID EQUALS expression'
+    ids[p[1]] = p[3]
+    
+def p_statement_expr(p):
+    'stmt : expression'
+    #print(p[1])
+
+def p_stmt_comparison(p):
+    'stmt : comparison'
+    p[0] = p[1]
+
+def p_stmt_if(p):
+    '''stmt : IF LPAREN BOOLEAN RPAREN stmt %prec IF
+            | IF LPAREN comparison RPAREN stmt %prec IF'''
+    if p[3] == True:  # p[3] contém o valor booleano da condição
+        p[0] = p[5]
+    else:
+        p[0] = None
+    
+
+def p_stmt_if_else(p):
+    '''stmt : IF LPAREN BOOLEAN RPAREN stmt ELSE stmt %prec IF
+            | IF LPAREN comparison RPAREN stmt ELSE stmt %prec IF'''
+    if p[3]:  # p[3] contém o valor booleano da condição
+        p[0] = p[5]
+    else:
+        p[0] = p[7]
+
+def p_stmt_while(p):
+    '''stmt : WHILE LPAREN BOOLEAN RPAREN stmt %prec WHILE
+            | WHILE LPAREN comparison RPAREN stmt %prec WHILE'''
+    while p[3]:  # p[3] contém o valor booleano da condição
+        p[0] = p[5]
+def p_comparison(p):
+    '''comparison : expression EQUAL expression
+               | expression NOTEQUAL expression
+               | expression GREATERTHAN expression
+               | expression LESSTHAN expression
+               | expression GREATEQ expression
+               | expression LESSEQ expression'''
+                  
+    if p[2] == '==':
+        p[0] = p[1] == p[3]
+    elif p[2] == '!=':
+        p[0] = p[1] != p[3]
+    elif p[2] == '>':
+        p[0] = p[1] > p[3]
+    elif p[2] == '<':
+        p[0] = p[1] < p[3]
+    elif p[2] == '>=':
+        p[0] = p[1] >= p[3]
+    elif p[2] == '<=':
+        p[0] = p[1] <= p[3]
+        
 def p_expression(p):
-    '''expression : expression PLUS term
-                  | expression MINUS term
-        term      : term TIMES factor
-                  | term DIVIDE factor'''
+    '''expression : expression PLUS expression
+                    | expression MINUS expression
+                    | expression TIMES expression
+                    | expression DIVIDE expression'''
     if p[2] == '+':
         p[0] = p[1] + p[3]
     elif p[2] == '-':
@@ -35,10 +95,6 @@ def p_expression(p):
     elif p[2] == '/':
         p[0] = p[1] / p[3]
 
-def p_expression_term(p):
-    'expression : term'
-    p[0] = p[1]
-
 '''def p_term(p):
     term : term TIMES factor
             | term DIVIDE factor
@@ -47,25 +103,58 @@ def p_expression_term(p):
     elif p[2] == '/':
         p[0] = p[1] / p[3]'''
 
-def p_term_factor(p):
-    'term : factor'
-    p[0] = p[1]
-
-def p_factor_num(p):
-    'factor : NUMBER'
-    p[0] = p[1]
-
-def p_factor_expr(p):
-    'factor : LPAREN expression RPAREN'
+def p_expression_uminus(p):
+    'expression : MINUS expression %prec UMINUS'
+    p[0] = -p[2]
+    
+def p_statement_print(p):
+    'stmt : PRINT LPAREN expression RPAREN'
+    print(p[3])
+    
+def p_expression_group(p):
+    'expression : LPAREN expression RPAREN'
     p[0] = p[2]
 
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = p[1]
+
+def p_expression_name(p):
+    'expression : ID'
+    try:
+        p[0] = ids[p[1]]
+    except LookupError:
+        print(f"Undefined id {p[1]!r}")
+        p[0] = 0
 def p_error(p):
     print("Syntax error in input!")
 
 parser = yacc.yacc(method='SLR')
+input_test = '''a = 2
+b = 3
+if(a > b) print(a)
+'''
 
-def run(data):
-    result = parser.parse(data)
-    return result
+input_test = '''a = 7
+b = a * 2
+print(a + b)
+if(a < b) print(a)
+'''
+result = parser.parse(input_test)
+
+while True:
+    try:
+        s = input('calc > ')
+    except EOFError:
+        break
+    if not s: continue
+    result = parser.parse(s)
+
+#isso aqui só roda se o server chamar com a tag PAR
+# é pra fazer isso no server, quando identificar PAR
+'''thread1 = threading.Thread(target = alguma coisa,)
+thread2 = threading.Thread(target = alguma coisa)
 
 
+thread1.start()
+thread2.start()'''
